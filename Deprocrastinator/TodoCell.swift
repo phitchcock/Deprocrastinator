4dd6178675dfaf6789041908eff8b0fd7c07f396
phitchcock <phitchcock@me.com>
2014-10-06 12:42:34 -0700

    added custom class for tablecell, items saving in array populating tableview

---
+++ Deprocrastinator/TodoCell.swift
@@ -0,0 +1,30 @@
+//
+//  TodoCell.swift
+//  Deprocrastinator
+//
+//  Created by Peter Hitchcock on 10/6/14.
+//  Copyright (c) 2014 Peter Hitchcock. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    //IBOutlets
+    @IBOutlet weak var todoCellLabel: UILabel!
+
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Deprocrastinator/ViewController.swift
+++ Deprocrastinator/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+
+    //Vars
+    var itemsArray:[String] = []
+
+    //IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var itemTextField: UITextField!
+
+    //IBActions
+    @IBAction func onAddButtonPressed(sender: AnyObject) {
+
+        var item = itemTextField.text
+        itemsArray.append(item)
+        tableView.reloadData()
+        itemTextField.endEditing(true)
+        itemTextField.text = ""
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +37,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return itemsArray.count
+    }
+
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell:TodoCell = tableView.dequeueReusableCellWithIdentifier("dCell", forIndexPath: indexPath) as TodoCell
+
+        cell.todoCellLabel.text = itemsArray[indexPath.row]
+
+        return cell
+    }
 
 }
 
